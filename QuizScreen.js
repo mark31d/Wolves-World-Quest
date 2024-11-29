620522a95a38690142d77c24144507a276e95214
mark31d <markdashevskiy31@gmail.com>
2024-11-29 18:29:07 +0200

    massage

--- QuizScreen.js
+++ QuizScreen.js
@@ -431,7 +431,7 @@ const QuizScreen = ({ navigation }) => {
       if (quizId) {
           loadQuestion();
       }
-  }, [quizId]);//component
+  }, [quizId]);//components
   const startQuiz = (selectedMode) => {
     setMode(selectedMode);
     setIsTimerActive(selectedMode === 'timed');
